b428e2d31eda09dc6b6edbded606c3f9ef22c02d
dhlee421 <dhlee421@gmail.com>
2021-12-15 21:57:29 +0900

    Add files via upload

---
+++ WPF/EnumExample.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EnumTest
+{
+    public enum Fruits
+    {
+        Apple,
+        Banana,
+        Orange,
+        Watermelon,
+        Mango,
+        Strawberry
+    }
+
+    enum Styles { Plaid = 0, Striped = 23, Tartan = 65, Corduroy = 78 };
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string fruit in Enum.GetNames(typeof(Fruits)))
+            {
+                Console.WriteLine(fruit);
+            }
+            Console.WriteLine("--------------------------------");
+
+            foreach (int fruit in Enum.GetValues(typeof(Fruits)))
+            {
+                Console.WriteLine(fruit);
+            }
+
+        }
+    }
+}
